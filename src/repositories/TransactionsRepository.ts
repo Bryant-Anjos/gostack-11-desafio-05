e397604fa5b100ba17cf051cfdc1a306555c7eab
Bryant Moreira dos Anjos <bryant_mx@hotmail.com>
2020-05-18 19:49:22 -0300

    Challenge complete

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,29 +1,67 @@
-import Transaction from '../models/Transaction';
+import Transaction from '../models/Transaction'
 
 interface Balance {
-  income: number;
-  outcome: number;
-  total: number;
+  income: number
+  outcome: number
+  total: number
+}
+
+interface TransactionDTO {
+  title: string
+  value: number
+  type: 'income' | 'outcome'
 }
 
 class TransactionsRepository {
-  private transactions: Transaction[];
+  private transactions: Transaction[]
 
   constructor() {
-    this.transactions = [];
+    this.transactions = []
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance: Balance = this.transactions.reduce(
+      (prevValue: Balance, transaction) => {
+        const { value } = transaction
+        const newValue = prevValue
+
+        if (transaction.type === 'income') {
+          newValue.income += value
+          newValue.total += value
+        }
+
+        if (transaction.type === 'outcome') {
+          newValue.outcome += value
+          newValue.total -= value
+        }
+
+        return newValue
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    )
+
+    return balance
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: TransactionDTO): Transaction {
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    })
+
+    this.transactions.push(transaction)
+
+    return transaction
   }
 }
 
-export default TransactionsRepository;
+export default TransactionsRepository
